8397b9af0e7de647d00355ec2b1eaaa2caddfda8
Kresna <satyakresna@unud.ac.id>
2021-12-08 11:16:04 +0800

    feat: get RSA256 public key signature

---
+++ src/Key.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace RistekUSDI\ServiceAccount;
+
+use RistekUSDI\ServiceAccount\Base;
+
+class Key
+{
+    private $token;
+    private $realm;
+
+    public function __construct()
+    {
+        $base = new Base();
+        $this->token = $base->getToken();
+        $this->realm = $base->getRealm();
+    }
+
+    public function get()
+    {
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $_ENV['SSO_BASE_URL']."/admin/realms/{$this->realm}/keys",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$this->token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        $result = [];
+
+        if ($httpcode === 200) {
+            $result = json_decode($response, true);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get RSA256 public key signature
+     */
+    public function getRSA256PublicKey()
+    {
+        $keys = $this->get();
+        $available_keys = $keys['keys'];
+        $public_key = null;
+
+        foreach ($available_keys as $value) {
+            if ($value['algorithm'] === 'RS256' && $value['use'] === 'SIG') {
+                $public_key = $value['publicKey'];
+            }
+        }
+
+        return $public_key;
+    }
+}
