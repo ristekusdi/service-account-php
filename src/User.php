8d4141c56103933650bd10550c2f3e38f81d2314
Kresna Satya <satyakresna@unud.ac.id>
2022-03-21 15:43:35 +0800

    feat: get user groups

--- src/User.php
+++ src/User.php
@@ -267,4 +267,35 @@ class User extends Base
 
         return $result;
     }
+
+    public function groups($user_id)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "{$this->getAdminRealmUrl()}/users/{$user_id}/groups",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$this->getToken()
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        $result = [];
+
+        if ($httpcode === 200) {
+            $result = json_decode($response, true);
+        }
+
+        return $result;
+    }
 }
